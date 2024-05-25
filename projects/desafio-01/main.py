55f3639f4f9a4e5fece90558378b0081bca5646d
dillonsilva <dillon_patrick@outlook.com>
2024-05-25 15:56:25 -0300

    função de adicionar contatos

--- projects/desafio-01/main.py
+++ projects/desafio-01/main.py
@@ -4,3 +4,15 @@ class Contatos:
         self.telefone = telefone
         self.email = email
         self.favorito = favorito
+
+    def adicionar_contato(self, lista_de_contatos):
+        contato = {
+            "nome": self.nome,
+            "telefone": self.telefone.replace(" ", ""),
+            "email": self.email,
+            "favorito": self.favorito,
+        }
+
+        lista_de_contatos.append(contato)
+        print(f"O contato '{self.nome}' foi adicionado(a).")
+        return
