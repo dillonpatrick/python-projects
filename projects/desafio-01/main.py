72c8a723c7f4190a0836e95bd903cd9ac68e2ded
dillonsilva <dillon_patrick@outlook.com>
2024-05-25 15:56:43 -0300

    função d listagem de contatos

--- projects/desafio-01/main.py
+++ projects/desafio-01/main.py
@@ -16,3 +16,21 @@ class Contatos:
         lista_de_contatos.append(contato)
         print(f"O contato '{self.nome}' foi adicionado(a).")
         return
+
+    def listar_contatos(self, lista_de_contatos):
+        print("\nLista de contatos")
+        print("-" * 45)
+        for indice, contato in enumerate(lista_de_contatos, start=1):
+            favorito = "⭐" if contato["favorito"] else " "
+            nome_contato = contato["nome"]
+            telefone_contato = contato["telefone"]
+            email_contato = contato["email"]
+            print(
+                f"{indice}"
+                f"\nFavorito: [{favorito}]"
+                f"\nNome: {nome_contato}"
+                f"\nTelefone: {telefone_contato}"
+                f"\nEmail: {email_contato}"
+            )
+            print("-" * 45)
+        return
