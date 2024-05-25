dd5d66f120a722a365da0306932c8d4eb938b2da
dillonsilva <dillon_patrick@outlook.com>
2024-05-25 18:33:49 -0300

    função para editar contato

--- projects/desafio-01/main.py
+++ projects/desafio-01/main.py
@@ -21,18 +21,24 @@ class Contatos:
         print("\nLista de contatos")
         print("-" * 45)
         for indice, contato in enumerate(lista_de_contatos, start=1):
-            favorito = "⭐" if contato["favorito"] else " "
+            favorito = "✔" if contato["favorito"] else " "
             nome_contato = contato["nome"]
             telefone_contato = contato["telefone"]
             email_contato = contato["email"]
             print(
-                f"{indice}"
+                f"Indice: {indice}"
                 f"\nFavorito: [{favorito}]"
                 f"\nNome: {nome_contato}"
                 f"\nTelefone: {telefone_contato}"
                 f"\nEmail: {email_contato}"
             )
             print("-" * 45)
+        return
+
+    def editar_contato(self, lista_de_contatos, indice_contato, chave, novo_valor):
+        indice = int(indice_contato) - 1
+        lista_de_contatos[indice].update({chave: novo_valor})
+        print(f"O contato foi atualizado(a).")
         return
 
 
@@ -59,13 +65,23 @@ while True:
             Contatos(nome_contato, telefone_contato, email_contato).adicionar_contato(
                 lista_de_contatos
             )
-
         else:
             print("O nome do contato é obrigatório.")
 
     elif escolha == "2":
         Contatos().listar_contatos(lista_de_contatos)
 
+    elif escolha == "3":
+        Contatos().listar_contatos(lista_de_contatos)
+        indice_contato = input("Digite o indice do contato a ser editado: ")
+        chave = input("Qual campo deseja atualizar (nome, email, telefone): ").lower()
+        if chave == "nome" or chave == "telefone" or chave == "email":
+            novo_valor = input("Digite o valor atualizado: ")
+            Contatos(nome_contato, telefone_contato, email_contato).editar_contato(
+                lista_de_contatos, indice_contato, chave, novo_valor
+            )
+        else:
+            print("Campo inválido.")
     elif escolha == "7":
         print("Programa finalizado")
         break
