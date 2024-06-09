343fdae47d9543f133b022c9aa3ad913e8d59e59
dillonsilva <dillon_patrick@outlook.com>
2024-06-09 18:44:25 -0300

    Adicionando a coluna de role

--- projects/modulo_04/sample_flask_auth/models/user.py
+++ projects/modulo_04/sample_flask_auth/models/user.py
@@ -5,4 +5,5 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default="user")
