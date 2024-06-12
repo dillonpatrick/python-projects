3f269837a1ae63c26b2bff08560c606b1a31dd11
dillonsilva <dillon_patrick@outlook.com>
2024-06-11 22:58:01 -0300

    rota de login

--- projects/modulo_04/desafio_02/app.py
+++ projects/modulo_04/desafio_02/app.py
@@ -27,5 +27,21 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        login_user(user)
+
+        if user and password == user.password:
+            return jsonify({"message": "User authenticated with success."})
+
+    return jsonify({"message": "Invalid credentials."}), 401
+
+
 if __name__ == "__main__":
     app.run(debug=True)
