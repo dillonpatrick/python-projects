bdc58062eb8d9be603029c1c4fa4f5bc821181ba
dillonsilva <dillon_patrick@outlook.com>
2024-06-15 18:56:08 -0300

    Rota de criação de pagamento

--- projects/modulo_05/app.py
+++ projects/modulo_05/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from datetime import datetime, timedelta
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
 
@@ -11,7 +12,21 @@ db.init_app(app)
 
 @app.route("/payments/pix", methods=["POST"])
 def create_payment():
-    return jsonify({"message": "The paymente has been created"})
+    data = request.get_json()
+
+    if "value" not in data:
+        return jsonify({"message": "Invalid value"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify(
+        {"message": "The payment has been created", "payment": new_payment.to_dict()}
+    )
 
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
