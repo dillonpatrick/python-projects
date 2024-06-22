8cbacb8220a87d659a266849384a5573bf79ee2a
dillonsilva <dillon_patrick@outlook.com>
2024-06-22 13:52:46 -0300

    Tratamento quando não encodar o id do pedido, e desconectar quando o pagamento for confimado

--- projects/modulo_05/app.py
+++ projects/modulo_05/app.py
@@ -76,6 +76,9 @@ def confirmation_pix():
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if not payment:
+        return render_template("404.html")
+
     if payment.paid:
         return render_template(
             "confirmed_payment.html",
@@ -95,6 +98,11 @@ def payment_pix_page(payment_id):
 @socketio.on("connect")
 def handle_connect():
     print("Client connected to the server.")
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client has disconnected to the server.")
 
 
 if __name__ == "__main__":
