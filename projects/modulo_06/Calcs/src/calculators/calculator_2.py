447d66f3f5944136d9ce32606355522be6333996
dillonsilva <dillon_patrick@outlook.com>
2024-07-01 21:49:06 -0300

    update

--- projects/modulo_06/Calcs/src/calculators/calculator_2.py
+++ projects/modulo_06/Calcs/src/calculators/calculator_2.py
@@ -1,13 +1,16 @@
 from typing import Dict, List
 from flask import request as FlaskRequest
+from src.drivers.numpy_handler import NumpyHandler
 
 
 class Calculator2:
     def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
         body = request.json
-        print(body)
         input_data = self._validate_body(body=body)
-        print(input_data)
+        calculated_number = self._process_data(input_data)
+        formated_response = self.__format_response(calculated_number)
+
+        return formated_response
 
     def _validate_body(self, body: Dict) -> List:
         if "numbers" not in body:
@@ -15,3 +18,12 @@ class Calculator2:
 
         input_data = body["numbers"]
         return input_data
+
+    def _process_data(self, input_data: List[float]) -> float:
+        numpy_handler = NumpyHandler()
+        firs_process_result = [(num * 11) ** 0.95 for num in input_data]
+        result = numpy_handler.standard_derivation(firs_process_result)
+        return 1 / result
+
+    def __format_response(self, calc_result: float) -> Dict:
+        return {"data": {"Calculator": 2, "result": round(calc_result, 2)}}
