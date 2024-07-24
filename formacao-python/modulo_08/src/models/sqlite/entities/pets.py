8dc38954f7b50b1a1fec04b6c83bd212e264f36d
dillonsilva <dillon_patrick@outlook.com>
2024-07-23 23:36:04 -0300

    Classes de ORM das tableas de pets e peoples

---
+++ formacao-python/modulo_08/src/models/sqlite/entities/pets.py
@@ -0,0 +1,14 @@
+from sqlalchemy import BIGINT, Column, String
+
+from src.models.sqlite.settings.base import Base
+
+
+class Pets(Base):
+    __tablename__ = "pets"
+
+    id = Column(BIGINT, primary_key=True, autoincrement="auto", unique=True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"Pets [name={self.name}, type={self.type}]"
