83053bef32aba3a0339f70240b5a433bf180dab9
dillonsilva <dillon_patrick@outlook.com>
2024-08-05 21:42:47 -0300

    Adicionando testes para a pets_repository

--- formacao-python/modulo_08/src/models/sqlite/repositories/pets_repository_test.py
+++ formacao-python/modulo_08/src/models/sqlite/repositories/pets_repository_test.py
@@ -1,6 +1,8 @@
 from unittest import mock
 
+import pytest
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 
 from src.models.sqlite.entities.pets import PetsTable
 
@@ -20,6 +22,21 @@ class MockConnection:
                 )
             ]
         )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+class MockConnectionNoResults:
+    def __init__(self) -> None:
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs):
+        raise NoResultFound("No result found")
 
     def __enter__(self):
         return self
@@ -49,3 +66,25 @@ def test_delete_pet():
     mock_connection.session.query.assert_called_once_with(PetsTable)
     mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
     mock_connection.session.delete.assert_called_once()
+
+
+def test_list_pets_no_result():
+    mock_connection = MockConnectionNoResults()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_not_called()
+    mock_connection.session.filter.assert_not_called()
+
+    assert response == []
+
+
+def test_delete_pet_error():
+    mock_connection = MockConnectionNoResults()
+    repo = PetsRepository(mock_connection)
+
+    with pytest.raises(Exception):
+        repo.delete_pets("petName")
+
+    mock_connection.session.rollback.assert_called_once()
