62a36fdabc5b4f9ddc0505efdac652d8ede4aadf
dillonsilva <dillon_patrick@outlook.com>
2024-08-17 14:51:53 -0300

    Implementando interfaces

---
+++ formacao-python/modulo_08/src/controllers/interfaces/pet_delete_controller.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class PetDeleteControllerInterface(ABC):
+
+    @abstractmethod
+    def delete(self, name: str) -> None:
+        pass
--- formacao-python/modulo_08/src/models/sqlite/interfaces/pets_repository.py
+++ formacao-python/modulo_08/src/models/sqlite/interfaces/pets_repository.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from src.models.sqlite.entities.pets import PetsTable
 
 
-class PetsRepository(ABC):
+class PetsRepositoryInterface(ABC):
 
     @abstractmethod
     def list_pets(self) -> list[PetsTable]:
