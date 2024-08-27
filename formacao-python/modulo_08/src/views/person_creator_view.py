f4f1d79d94ee60d53b80a47dcd67a090a4495eee
dillonsilva <dillon_patrick@outlook.com>
2024-08-26 22:13:40 -0300

    Criando views do projeto

--- formacao-python/modulo_08/src/views/person_creator_view.py
+++ formacao-python/modulo_08/src/views/person_creator_view.py
@@ -1,11 +1,13 @@
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from .interfaces.view_interface import ViewInterface
-from src.controllers.person_creator_controller import PersonCreatorController
+from src.controllers.interfaces.person_creator_controller import (
+    PersonCreatorControllerInterface,
+)
 
 
 class PersonCreatorView(ViewInterface):
-    def __init__(self, controller: PersonCreatorController) -> None:
+    def __init__(self, controller: PersonCreatorControllerInterface) -> None:
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
---
+++ formacao-python/modulo_08/src/views/pet_lister_view.py
@@ -0,0 +1,16 @@
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+from src.controllers.interfaces.pet_lister_controller import (
+    PetListerControllerInterface,
+)
+
+
+class PetListerView(ViewInterface):
+    def __init__(self, controller: PetListerControllerInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        body_response = self.__controller.list()
+
+        return HttpResponse(status_code=200, body=body_response)
