6b51330533d02948cf5897fb8d65a23f68ff93d4
dillonsilva <dillon@niuco.com.br>
2024-09-22 11:40:39 -0300

    update

--- formacao-python/modulo_08/src/main/routes/pets_routes.py
+++ formacao-python/modulo_08/src/main/routes/pets_routes.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, Flask, jsonify
+from flask import Blueprint, jsonify
+from src.views.http_types.http_request import HttpRequest
+
+from src.main.composer.pet_lister_composer import pet_lister_composer
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
-    return jsonify({"Ola": "Mundo"})
+    http_request = HttpRequest()
+    view = pet_lister_composer()
+
+    http_response = view.handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
